4d1ba9e410ef0b8599b56b3555a98bf05ff71572
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-26 22:43:26 +0200

    created theme

--- src/common/theme.js
+++ src/common/theme.js
@@ -3,3 +3,13 @@ const colorCollection = {
   blueBayoux: "#4F6272",
   snowDrift: "#F2F7F2",
 };
+
+const theme = {
+  colors: {
+    background: {
+      box: colorCollection.riverBed,
+      page: colorCollection.blueBayoux,
+    },
+    text: colorCollection.snowDrift,
+  },
+};
