e2d72ad4d4855d4afad0d317385ff4c21973bfcc
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-06 22:37:17 +0100

    added store to index

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import App from "./core/App";
 import { GlobalStyle } from "./core/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./common/theme";
+import store from "./core/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
