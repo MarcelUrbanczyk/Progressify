04ad6d50abc9fb836938f7da50304b2b585e5958
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-07 23:14:56 +0100

    reorganised app

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,8 +1,17 @@
-import MobileApp from "../features/Mobile/index";
+import Navigation from "../features/Navigation/index";
+import { Main } from "../common/Main";
+import ProjectList from "../features/Projects/ProjectList/index";
 import React from "react";
 
 const App = () => {
-  return <MobileApp />;
+  return (
+    <>
+      <Navigation />
+      <Main>
+        <ProjectList />
+      </Main>
+    </>
+  );
 };
 
 export default App;
