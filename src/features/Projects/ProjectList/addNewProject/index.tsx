798e02f75ab95c952a606a91bb5ee3c9e4e16969
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-08 23:31:36 +0100

    added project creation form

--- src/features/Projects/ProjectList/addNewProject/index.tsx
+++ src/features/Projects/ProjectList/addNewProject/index.tsx
@@ -1,12 +1,69 @@
 import PlusIcon from "./plusIcon";
-import { Wrapper, Button, Paragraph } from "./styled";
+import MinusIcon from "./minusIcon";
+import {
+  Wrapper,
+  Button,
+  Paragraph,
+  Divider,
+  Form,
+  Label,
+  Input,
+} from "./styled";
+import { useState } from "react";
 import React from "react";
 
-export default () => (
-  <Wrapper>
-    <Button>
-      <PlusIcon />
-    </Button>
-    <Paragraph>Add new project</Paragraph>
-  </Wrapper>
-);
+export default () => {
+  const [addNewProject, setAddNewProject] = useState(true);
+  const [projectName, setProjectName] = useState("");
+  const [projectDeadline, setProjectDeadline] = useState("");
+
+  if (!addNewProject) {
+    return (
+      <Wrapper>
+        <Button
+          onClick={() => {
+            setAddNewProject(!addNewProject);
+          }}
+        >
+          {!addNewProject ? <PlusIcon /> : <MinusIcon />}
+        </Button>
+        <Paragraph>Add new project</Paragraph>
+      </Wrapper>
+    );
+  }
+  return (
+    <>
+      <Wrapper>
+        <Button
+          onClick={() => {
+            setAddNewProject(!addNewProject);
+          }}
+        >
+          {!addNewProject ? <PlusIcon /> : <MinusIcon />}
+        </Button>
+        <Paragraph>Project creation</Paragraph>
+      </Wrapper>
+      <Divider />
+      <Form>
+        <Label>Project name: </Label>
+        <Input
+          type="text"
+          required
+          value={projectName}
+          onChange={(event) => {
+            setProjectName(event.target.value);
+          }}
+        />
+        <Label>Deadline: </Label>
+        <Input
+          type="date"
+          required
+          value={projectDeadline}
+          onChange={(event) => {
+            setProjectDeadline(event.target.value);
+          }}
+        />
+      </Form>
+    </>
+  );
+};
