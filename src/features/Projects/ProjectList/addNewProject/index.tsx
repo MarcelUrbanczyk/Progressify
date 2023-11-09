71709b3a2c2d05f17d29ee9d6ce1492315ff601a
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-09 23:21:17 +0100

    added task creation for new project

--- src/features/Projects/ProjectList/addNewProject/index.tsx
+++ src/features/Projects/ProjectList/addNewProject/index.tsx
@@ -2,31 +2,55 @@ import PlusIcon from "./plusIcon";
 import MinusIcon from "./minusIcon";
 import {
   Wrapper,
-  Button,
+  ProjectButton,
   Paragraph,
   Divider,
   Form,
   Label,
   Input,
+  TaskButton,
+  TaskCreatorWrapper,
+  SubmitButton,
+  TaskWrapper,
 } from "./styled";
 import { useState } from "react";
 import React from "react";
+import { AlternativeBox } from "../../../../common/alternativeBox";
+import TrashBinIcon from "./trashbinIcon";
 
 export default () => {
+  interface Task {
+    name: string;
+    isDone: boolean;
+  }
+
   const [addNewProject, setAddNewProject] = useState(true);
   const [projectName, setProjectName] = useState("");
   const [projectDeadline, setProjectDeadline] = useState("");
+  const [addNewTask, setAddNewTask] = useState(false);
+  const [taskName, setTaskName] = useState("");
+  const [tasks, setTasks] = useState([] as Task[]);
+
+  console.log(tasks);
+
+  const addTask = () => {
+    if (taskName !== "") {
+      setTasks([...tasks, { name: taskName.trim(), isDone: false }]);
+      setTaskName("");
+      setAddNewTask(false);
+    }
+  };
 
   if (!addNewProject) {
     return (
       <Wrapper>
-        <Button
+        <ProjectButton
           onClick={() => {
             setAddNewProject(!addNewProject);
           }}
         >
           {!addNewProject ? <PlusIcon /> : <MinusIcon />}
-        </Button>
+        </ProjectButton>
         <Paragraph>Add new project</Paragraph>
       </Wrapper>
     );
@@ -34,13 +58,13 @@ export default () => {
   return (
     <>
       <Wrapper>
-        <Button
+        <ProjectButton
           onClick={() => {
             setAddNewProject(!addNewProject);
           }}
         >
           {!addNewProject ? <PlusIcon /> : <MinusIcon />}
-        </Button>
+        </ProjectButton>
         <Paragraph>Project creation</Paragraph>
       </Wrapper>
       <Divider />
@@ -64,6 +88,46 @@ export default () => {
           }}
         />
       </Form>
+
+      <AlternativeBox>
+        <TaskCreatorWrapper>
+          <TaskButton
+            onClick={() => {
+              setAddNewTask(!addNewTask);
+            }}
+          >
+            {!addNewTask ? <PlusIcon /> : <MinusIcon />}{" "}
+          </TaskButton>
+          <Paragraph>{!addNewTask ? "Add new task" : "Set task"}</Paragraph>
+        </TaskCreatorWrapper>
+        {addNewTask && (
+          <Form>
+            <Label>Task name: </Label>
+            <Input
+              type="text"
+              required
+              value={taskName}
+              onChange={(event) => {
+                setTaskName(event.target.value);
+              }}
+            />
+            <SubmitButton onClick={addTask}>Add task</SubmitButton>
+          </Form>
+        )}
+      </AlternativeBox>
+      {tasks.length > 0 &&
+        tasks.map((task) => (
+          <AlternativeBox>
+            <TaskWrapper>
+              <Paragraph>{task.name}</Paragraph>
+              <TrashBinIcon
+                onClick={() => {
+                  setTasks(tasks.filter((t) => t.name !== task.name));
+                }}
+              />
+            </TaskWrapper>
+          </AlternativeBox>
+        ))}
     </>
   );
 };
