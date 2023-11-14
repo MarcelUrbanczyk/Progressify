03a7fab8bc4c6ae9479ba095b1eb47e8d9ab8690
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-14 22:42:40 +0100

    styled projects and created animation for long title

--- src/features/Projects/ProjectList/styled.ts
+++ src/features/Projects/ProjectList/styled.ts
@@ -1,7 +1,58 @@
-import { styled } from "styled-components";
+import { keyframes, styled } from "styled-components";
+
+const slideAnimation = keyframes` 
+  0% {
+    transform: translateX(0);
+  }
+  20%{
+    transform: translateX(-100%);
+  }
+  40%{
+    transform: translateX(-100%);
+  }
+  60%{
+    transform: translateX(0);
+  }
+  100%{
+    transform: translateX(0);  
+  }
+`;
+
+interface TitleProps {
+  slide?: boolean;
+}
 
 export const Section = styled.section`
   display: flex;
   flex-direction: column;
+  gap: 20px;
+`;
+
+export const Title = styled.h2<TitleProps>`
+  color: ${({ theme }) => theme.colors.text};
+  margin: 0;
+  font-weight: 400;
+  font-size: 18px;
+  animation: ${({ slide }) => (slide ? slideAnimation : "")} 8s linear infinite;
+`;
+
+export const TitleWrapper = styled.span`
+  overflow: hidden;
+`;
+
+export const DaysLeft = styled.p`
+  color: ${({ theme }) => theme.colors.text};
+  margin: 0;
+  font-weight: 300;
+  font-size: 16px;
+  margin-top: 3px;
+`;
+
+export const TasksCompleted = styled(DaysLeft)``;
+
+export const ProjectWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 3fr 1fr 1fr;
+  align-items: center;
   gap: 20px;
 `;
