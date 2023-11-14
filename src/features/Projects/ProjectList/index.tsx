9b69071aef87ae6b2063f98f7629a4e2c9d08c9f
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-14 22:42:22 +0100

    project list is now mapping every project

--- src/features/Projects/ProjectList/index.tsx
+++ src/features/Projects/ProjectList/index.tsx
@@ -1,13 +1,45 @@
 import React from "react";
 import { Box } from "../../../common/box";
-import { Section } from "./styled";
+import {
+  Section,
+  Title,
+  DaysLeft,
+  ProjectWrapper,
+  TasksCompleted,
+  TitleWrapper,
+} from "./styled";
 import AddNewProject from "./addNewProject";
+import { useAppSelector } from "../../reduxHooks";
+import getDaysLeft from "../../getDaysLeft";
+import ArrowToRight from "./arrowToRight";
 
-const ProjectList = () => (
-  <Section>
-    <AddNewProject />
-    <Box></Box>
-  </Section>
-);
+const ProjectList = () => {
+  const projects = useAppSelector((state) => state.projects);
+  return (
+    <Section>
+      <AddNewProject />
+      {projects.map((project) => (
+        <Box key={project.id}>
+          <ProjectWrapper>
+            <TitleWrapper>
+              <Title slide={project.name.length >= 9}>{project.name}</Title>
+            </TitleWrapper>
+            <DaysLeft>
+              {getDaysLeft(project.deadline) <= 0
+                ? "Expired"
+                : getDaysLeft(project.deadline) + " days left"}
+            </DaysLeft>
+            <TasksCompleted>
+              {project.tasks.filter((task) => task.isDone).length +
+                "/" +
+                project.tasks.length}
+            </TasksCompleted>
+            <ArrowToRight />
+          </ProjectWrapper>
+        </Box>
+      ))}
+    </Section>
+  );
+};
 
 export default ProjectList;
