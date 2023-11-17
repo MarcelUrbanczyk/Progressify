582389b66d392687b00d9446b23be53fdfa5279a
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-17 23:39:37 +0100

    added more styles to main

--- src/common/Main.ts
+++ src/common/Main.ts
@@ -1,5 +1,16 @@
 import { styled } from "styled-components";
 
 export const Main = styled.main`
+  margin: 0 auto;
   padding: 20px;
+  max-width: 1400px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktopMid}) {
+    max-width: 1000px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    max-width: unset;
+  }
 `;
