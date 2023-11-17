683d78b7f3e8d7187408af7a0274c945cbcc3237
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-17 23:40:24 +0100

    renamed navigation to mobile navigation

---
+++ src/features/MobileNavigation/index.tsx
@@ -0,0 +1,18 @@
+import { Header, StyledNavigation, Link } from "./styled";
+import React from "react";
+import Menu from "./menu";
+import Options from "./options";
+
+const MobileNavigation = () => (
+  <StyledNavigation>
+    <Link>
+      <Menu />
+    </Link>
+    <Header>Projectize</Header>
+    <Link>
+      <Options />
+    </Link>
+  </StyledNavigation>
+);
+
+export default MobileNavigation;
