7e243dd2752a9c91505c24526bf5ad230088ebc8
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-17 23:40:06 +0100

    desktop and mobile navigation are now separate components

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,12 +1,14 @@
-import Navigation from "../features/Navigation/index";
+import MobileNavigation from "../features/MobileNavigation/index";
 import { Main } from "../common/Main";
 import ProjectList from "../features/Projects/ProjectList/index";
 import React from "react";
+import Navigation from "../features/Navigation";
 
 const App = () => {
   return (
     <>
       <Navigation />
+      <MobileNavigation />
       <Main>
         <ProjectList />
       </Main>
