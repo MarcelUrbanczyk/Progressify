ae94af2ffd940eea7064acc748bfc6ea37005b5c
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-11-17 23:41:01 +0100

    moved files to mobile navigation folder

---
+++ src/features/MobileNavigation/styled.ts
@@ -0,0 +1,26 @@
+import { styled } from "styled-components";
+
+export const Header = styled.h1`
+  color: ${({ theme }) => theme.colors.text};
+  font-family: "Mitr";
+  font-size: 32px;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const StyledNavigation = styled.nav`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background.box};
+  box-shadow: ${({ theme }) => theme.shadows.nav};
+  display: none;
+  justify-content: space-between;
+  padding: 10px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    display: flex;
+  }
+`;
+
+export const Link = styled.a`
+  margin: 14px 10px 10px;
+  padding: 0;
+`;
