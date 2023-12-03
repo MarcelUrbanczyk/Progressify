57cbfff7bfd146499b8bcaae6e2d3b1b02787883
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-03 20:34:55 +0100

    created routes

---
+++ src/features/routes.ts
@@ -0,0 +1,5 @@
+export const toProjects = () => "/projects";
+export const toProject = () => "/projects/:id";
+export const toGoals = () => "/goals";
+export const toGoal = () => "/goals/:id";
+export const toTasks = () => "/tasks";
