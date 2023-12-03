9e5f888c6737f698abff2aaeaa7f29aae7dc0403
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-03 20:35:28 +0100

    implemented react router

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,16 +1,22 @@
 import { Main } from "../common/Main";
 import ProjectList from "../features/Projects/ProjectList/index";
 import Navigation from "../features/Navigation/index";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import React from "react";
+import { toProjects } from "../features/routes";
 
 const App = () => {
   return (
-    <>
+    <HashRouter>
       <Navigation />
       <Main>
-        <ProjectList />
+        <Switch>
+          <Route path={toProjects}>
+            <ProjectList />
+          </Route>
+        </Switch>
       </Main>
-    </>
+    </HashRouter>
   );
 };
 
