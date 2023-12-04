ed4ee0e078794f015f85c22eaf193f1df809ebec
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-04 23:27:44 +0100

    implemented react router to navigation

--- src/features/Navigation/Desktop/index.tsx
+++ src/features/Navigation/Desktop/index.tsx
@@ -6,19 +6,39 @@ import {
   NavLinkWrapper,
   NavLink,
 } from "./styled";
+import { useLocation } from "react-router-dom";
+import { toProjects, toTasks, toGoals } from "../../routes";
 
-const DesktopNavigation = () => (
-  <StyledNavigation>
-    <Wrapper>
-      <Header>Projectize</Header>
-      <NavLinkWrapper>
-        <NavLink>Projects</NavLink>
-        <NavLink>Daily Tasks</NavLink>
-        <NavLink>Goals</NavLink>
-        <NavLink>Settings</NavLink>
-      </NavLinkWrapper>
-    </Wrapper>
-  </StyledNavigation>
-);
+const DesktopNavigation = () => {
+  const location = useLocation();
+  return (
+    <StyledNavigation>
+      <Wrapper>
+        <Header>Projectize</Header>
+        <NavLinkWrapper>
+          <NavLink
+            to={toProjects()}
+            active={location.pathname.startsWith("/projects")}
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to={toTasks()}
+            active={location.pathname.startsWith("/tasks")}
+          >
+            Daily Tasks
+          </NavLink>
+          <NavLink
+            to={toGoals()}
+            active={location.pathname.startsWith("/goals")}
+          >
+            Goals
+          </NavLink>
+          <NavLink>Settings</NavLink>
+        </NavLinkWrapper>
+      </Wrapper>
+    </StyledNavigation>
+  );
+};
 
 export default DesktopNavigation;
