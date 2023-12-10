3cb71f4a60da5ec6c868148b029dee452457305b
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-10 14:00:19 +0100

    fixed routes errors

--- src/features/routes.ts
+++
@@ -1,5 +0,0 @@
-export const toProjects = () => "/projects";
-export const toProject = () => "/projects/:id";
-export const toGoals = () => "/goals";
-export const toGoal = () => "/goals/:id";
-export const toTasks = () => "/tasks";
