6e6e36552c8bedb01822191d68179ef6ad35e29b
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-16 23:38:54 +0100

    added settings route

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -3,3 +3,4 @@ export const toProject = "/projects/:id";
 export const toGoals = "/goals";
 export const toGoal = "/goals/:id";
 export const toTasks = "/tasks";
+export const toSettings = "/settings";
