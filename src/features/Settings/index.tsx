4a1a8febf972675e8e9c68171e1d157cada514b5
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-18 23:57:17 +0100

    added more components and state

--- src/features/Settings/index.tsx
+++ src/features/Settings/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Box, Paragraph, Wrapper } from "./styled";
+import { useState } from "react";
+import { Box, Paragraph, Wrapper, Button, AnimationWrapper } from "./styled";
+import ArrowToRight from "../../common/svg/arrowToRight";
 
-const Settings = () => (
-  <Wrapper>
-    <Box>
-      <Paragraph>Theme</Paragraph>
-    </Box>
-  </Wrapper>
-);
+const Settings = () => {
+  const [areThemeSettingsOpened, setAreThemeSettingsOpened] = useState(false);
+  const toggleAreThemeSettingsOpened = () => {
+    setAreThemeSettingsOpened(!areThemeSettingsOpened);
+  };
+  return (
+    <Wrapper>
+      <Box>
+        <Paragraph>Theme</Paragraph>
+        <Button onClick={toggleAreThemeSettingsOpened}>
+          <AnimationWrapper $flip={areThemeSettingsOpened}>
+            <ArrowToRight />
+          </AnimationWrapper>
+        </Button>
+      </Box>
+    </Wrapper>
+  );
+};
 
 export default Settings;
