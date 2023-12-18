a60f493a0fe68b11ec314e1d05b7cf6f81aa8cf3
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-18 23:57:25 +0100

    added animation wrapper

--- src/features/Settings/styled.ts
+++ src/features/Settings/styled.ts
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import { styled, css } from "styled-components";
 import { BoxStyles } from "../../common/styles/box";
+import { SquareButtonStyle } from "../../common/styles/button";
 
 export const Wrapper = styled.section`
   display: grid;
@@ -9,9 +10,24 @@ export const Wrapper = styled.section`
 export const Box = styled.div`
   ${BoxStyles}
   padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const Paragraph = styled.p`
   margin: 0;
   font-size: 20px;
+`;
+
+export const AnimationWrapper = styled.span<{ $flip?: boolean }>`
+  transition: 0.3s;
+  transform: rotate(90deg);
+  ${({ $flip }) => css`
+    transform: rotate(90deg);
+  `}
+`;
+
+export const Button = styled.button`
+  ${SquareButtonStyle}
 `;
