401b08be3bd92d57e286abbd855029100ee1b0ee
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-18 23:44:13 +0100

    created box for settings

--- src/features/Settings/styled.ts
+++ src/features/Settings/styled.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
+import { BoxStyles } from "../../common/styles/box";
 
 export const Wrapper = styled.section`
   display: grid;
   grid-template-columns: auto auto;
+`;
+
+export const Box = styled.div`
+  ${BoxStyles}
 `;
