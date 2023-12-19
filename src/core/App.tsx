07ee0e7a8a179963812758605d13bb94e80e78a1
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-19 23:17:59 +0100

    added project page to app

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,9 +1,10 @@
 import { Main } from "../common/Main";
+import ProjectPage from "../features/Projects/ProjectPage";
 import ProjectList from "../features/Projects/ProjectList/index";
 import Navigation from "../features/Navigation";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import React from "react";
-import { toGoals, toProjects, toSettings, toTasks } from "./routes";
+import { toGoals, toProject, toProjects, toSettings, toTasks } from "./routes";
 import Settings from "../features/Settings";
 
 const App = () => {
@@ -12,6 +13,9 @@ const App = () => {
       <Navigation />
       <Main>
         <Switch>
+          <Route path={toProject}>
+            <ProjectPage />
+          </Route>
           <Route path={toProjects}>
             <ProjectList />
           </Route>
