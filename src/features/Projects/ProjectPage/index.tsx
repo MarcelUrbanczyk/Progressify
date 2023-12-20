ba4cf6b986b360c265939ec3c15288c5e058741a
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-12-20 22:42:33 +0100

    created styled components for project page

--- src/features/Projects/ProjectPage/index.tsx
+++ src/features/Projects/ProjectPage/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { getProjectById } from "../projectSlice";
+import { useAppSelector } from "../../reduxHooks";
+import { Section, Title, Wrapper } from "./styled";
 
 const ProjectPage = () => {
   const { id } = useParams();
-  console.log(id);
-  return <span></span>;
+  const project = useAppSelector((state) => getProjectById(state, id));
+  console.log(project);
+  return (
+    <Section>
+      <Wrapper>
+        <Title>{project.name}</Title>
+      </Wrapper>
+    </Section>
+  );
 };
 
 export default ProjectPage;
---
+++ src/features/Projects/ProjectPage/styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+`;
+
+export const Wrapper = styled.span``;
+
+export const Title = styled.h2`
+  font-weight: 400;
+  font-size: 48px;
+`;
